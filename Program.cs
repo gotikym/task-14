7769b335a5e097bec63bdb3698252eb094a29bbf
One <One@DESKTOP-MIB22JC>
2022-05-01 14:47:18 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        byte appRaitingFromUser;
+        string userMail;
+        Console.WriteLine("Добро пожаловать на тестирование нашей программы, " +
+            "чтобы зарегестрироваться, введите ваше имя и придумайте пароль.");
+        Console.Write("Имя: ");
+        string registrationUserName = Console.ReadLine();
+        Console.Write("Пароль: ");
+        string registrationUserPassword = Console.ReadLine();
+        Console.Clear();
+        Console.WriteLine("Вы успешно зарегестрировались, теперь войдите в свой аккаунт.");
+        Console.Write("Введите ваше имя: ");
+        string userInputName = Console.ReadLine();
+        Console.Write("Введите ваш пароль: ");
+        string userInputPassword = Console.ReadLine();
+        Console.Clear();
+
+        if (userInputName == registrationUserName && userInputPassword == registrationUserPassword)
+        {
+            Console.WriteLine("Перейти в раздел меню - нажмите Enter, выйти из приложения напишите - Esc");
+
+            while (Console.ReadLine() != "Esc")
+            {
+                Console.WriteLine("Menu:");
+                Console.WriteLine("AppEvaluation - оценить наше приложение");
+                Console.WriteLine("AddMail - добавить вашу почту, для уведомления обновлений и новостей по нашему приложению");
+                Console.WriteLine("ChangeName -  изменить имя");
+                Console.WriteLine("MyName -  узнать ваше имя)");
+                Console.WriteLine("ChangePassword - изменить пароль \n");
+                string userChoice = Console.ReadLine();
+
+                switch (userChoice)
+                {
+                    case "AppEvaluation":
+                        Console.WriteLine("Оцените наше приложение по 5-ти бальной шкале: ");
+                        appRaitingFromUser = Convert.ToByte(Console.ReadLine());
+                        Console.WriteLine("Спасибо за вашу оценку");
+                        Console.WriteLine("Перейти в раздел меню - нажмите Enter, выйти из приложения напишите - Esc");
+                        break;
+
+                    case "AddMail":
+                        Console.Write("Введите ваш e-mail: ");
+                        userMail = Console.ReadLine();
+                        Console.WriteLine("Теперь вам на почту будут приходить уведомления о нашем приложении.");
+                        Console.WriteLine("Перейти в раздел меню - нажмите Enter, выйти из приложения напишите - Esc");
+                        break;
+
+                    case "ChangeName":
+                        Console.WriteLine("Введите ваше прежне имя: ");
+                        string oldUserName = Console.ReadLine();
+                        if (oldUserName == registrationUserName)
+                        {
+                            Console.Write("Введите новое имя: ");
+                            registrationUserName = Console.ReadLine();
+                            Console.WriteLine("Перейти в раздел меню - нажмите Enter, выйти из приложения напишите - Esc");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Вас не так зовут :Р");
+                            Console.WriteLine("Перейти в раздел меню - нажмите Enter, выйти из приложения напишите - Esc");
+                        }
+                        break;
+
+                    case "MyName":
+                        Console.WriteLine("Ваше имя - " + registrationUserName);
+                        Console.WriteLine("Перейти в раздел меню - нажмите Enter, выйти из приложения напишите - Esc");
+                        break;
+
+                    case "ChangePassword":
+                        Console.WriteLine("Введите ваш прежний пароль: ");
+                        string oldUserPassword = Console.ReadLine();
+                        if (oldUserPassword == registrationUserPassword)
+                        {
+                            Console.Write("Введите новый пароль: ");
+                            registrationUserPassword = Console.ReadLine();
+                            Console.WriteLine("Перейти в раздел меню - нажмите Enter, выйти из приложения напишите - Esc");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Пароль не совпадает с предыдущим :Р");
+                            Console.WriteLine("Перейти в раздел меню - нажмите Enter, выйти из приложения напишите - Esc");
+                        }
+                        break;
+                }
+            }
+
+        }
+        else
+        {
+            Console.WriteLine("Имя или пароль введены не верно");
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("task 14")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("task 14")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("a9b65c59-e9e6-44e5-b792-22d9a6465134")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
